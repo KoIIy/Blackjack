c42fe446f2c83210c1fa7f019dde203d5422664b
KoIIy <koiiy03@mail.ru>
2025-12-17 18:57:30 +0300

    реализован основной функционал игры Blackjack

---
+++ Blackjack/Blackjack/Blackjack.cpp
@@ -0,0 +1,158 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <random>
+
+using namespace std;
+
+struct Cards {
+    string rank;
+    string suit;
+    int value;
+};
+class Card {
+private:
+    vector<string> suits = { "Черви", "Бубны", "Пики", "Крести" };
+    vector<string> ranks = { "2", "3", "4", "5", "6", "7", "8", "9", "10", "Валет", "Дама", "Король", "Туз" };
+	int values[13] = { 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 1 };
+    
+public:
+    vector<Cards> createDeck() {
+        vector<Cards> deck;
+        for (const auto& suit : suits) {
+            int i = 0;
+            for (const auto& rank : ranks) {
+                deck.push_back({ rank,suit,values[i] });
+                i++;
+            }
+        }
+        return deck;
+    }
+};
+
+class Game {
+private:
+    vector<Cards> deck,player,dealer;
+    int indexCard = 0; //Индекс следующей карты
+public:
+    void mixDeck() {
+        shuffle(deck.begin(), deck.end(), default_random_engine(time(0)));
+        return;
+    }
+    Cards takeCard() {
+        if (indexCard >= deck.size()) {
+            mixDeck();
+            indexCard = 0;
+        }
+        return deck[indexCard++];
+    }
+    int checkHand(vector<Cards>& hand) {
+        int countAce = 0;
+        int sum = 0;
+        for (const auto& card : hand) {
+            sum += card.value;
+            if (card.rank == "Туз") {
+                countAce++;
+            }
+        }
+        while (countAce  > 0 && sum + 10 <= 21) {
+            sum += 10;
+            countAce--;
+        }
+        return sum;
+    }
+    void startGame() {
+        while (true) {
+            player.clear();
+            dealer.clear();
+            bool over = false; //перебор
+            player.push_back(takeCard());
+            dealer.push_back(takeCard());
+            player.push_back(takeCard());
+            dealer.push_back(takeCard());
+            cout << "Первая карта дилера:" << endl;
+            cout << dealer[0].rank << "\t" << dealer[0].suit << endl;
+            cout << endl;
+            while (true) {
+                cout << "Ваши карты:" << endl;
+                for (const auto& card : player) {
+                    cout << card.rank << "\t" << card.suit << endl;
+                }
+                cout << "Очки: " << checkHand(player) << endl;
+                if (checkHand(player) > 21) {
+                    over = true;
+                    break;
+                }
+                cout << "Взять карту? (y/n):";
+                char choice;
+                cin >> choice;
+                cout << endl;
+                if (choice == 'y') {
+                    player.push_back(takeCard());
+                }
+                else {
+                    break;
+                }
+            }
+            if (!over) {
+                while (checkHand(player)>checkHand(dealer)) {
+                    dealer.push_back(takeCard());
+                }
+            }
+            cout << "Карты дилера:" << endl;
+            for (const auto& card : dealer) {
+                cout << card.rank << "\t" << card.suit << endl;
+            }
+            cout << "Очки: " << checkHand(dealer) << endl;
+            cout << endl;
+            int p = checkHand(player);
+            int d = checkHand(dealer);
+            if (p<=21&&(p > d || d > 21))
+            {
+                cout << "Поздравляю вы выиграли!" << endl;
+            } 
+            else
+            {
+                if (p == d) {
+                    cout << "Ничья." << endl;
+                }
+                else 
+                {
+                    cout << "Вы проиграли." << endl;
+                }
+            }
+            cout << endl;
+            cout << "Продолжить? (y/n):";
+            char choice;
+            cin >> choice;
+            cout << endl;
+            system("cls");
+            if (choice == 'n') {
+                break;
+            }
+            
+        }
+    }
+    Game(vector<Cards> _deck) : deck(_deck) {
+        mixDeck();
+    }
+
+
+
+};
+
+int main()
+{
+    system("chcp 1251");
+    cout << endl;
+    cout << "Добро пожаловать в игру Блэкджек!" << endl;
+    cout << endl;
+	int meCounter, dealerCounter = 0;
+    Card card;
+    Game game(card.createDeck());
+    game.startGame();
+
+    //cout << "Ваш баланс = " << endl;
+
+}
